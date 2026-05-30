16bb9c09d9afe3ad0734c073d1c41744f1c91ad8
Nishant9518 <sharmaji9518220885@gmail.com>
2026-05-30 22:36:34 +0530

    l

--- intro.js
+++ intro.js
@@ -1,1 +1,24 @@
 console.log("hello world")
+let a = 10;
+let b = 20;
+let sum = a +b;
+console.log("the sum of a and b is " + sum);
+console.log(typeof sum);
+let name = "Nishant";
+console.log("hello " + name);
+let d = true;
+console.log(typeof d);
+let e = null;
+console.log(typeof e);
+let g = undefined;
+console.log(typeof g);
+let h = symbol("hello");
+console.log(typeof h);
+
+const item ={
+    "harry": true,
+    "pric": false,
+    "nishant": 67,
+}
+
+console.log(item["nishant"]);
