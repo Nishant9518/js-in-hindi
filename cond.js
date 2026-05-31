ecbfc939bddf285ac8366bb9200ee1af17a39164
Nishant9518 <sharmaji9518220885@gmail.com>
2026-05-31 11:16:27 +0530

    n

---
+++ cond.js
@@ -0,0 +1,12 @@
+/*let a = 18;
+a = Number.parseInt(a);
+if (a <=0) {
+    console.log("invalid input")
+}
+else if (a >0 && a < 18) {
+    console.log("you are a kid")
+}
+else {
+    console.log("you are an adult")
+} */
+let b = prompt("hey what is your age");
